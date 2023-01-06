ef7d1842d4b4c16c49ac8ed14a6b9bd3b29bf92a
Evgeniy Bulavin <vvp835@yandex.ru>
2023-01-06 07:20:28 +0400

    Format the code a bit

--- lib/annotate_controllers/inspector.rb
+++ lib/annotate_controllers/inspector.rb
@@ -2,22 +2,18 @@ require 'action_dispatch/routing/inspector'
 
 module AnnotateControllers
   class Inspector
-
     class << self
-
       def map_all_routes
         all_routes = Rails.application.routes.routes
         inspector = ActionDispatch::Routing::RoutesInspector.new(all_routes)
-        remove_constraints(
-          inspector.format(ActionDispatch::Routing::ConsoleFormatter::Sheet.new).split("\n").drop(1)
-        )
+        formatter = ActionDispatch::Routing::ConsoleFormatter::Sheet.new
+        routes = inspector.format(formatter).split("\n").drop(1)
+        remove_constraints(routes)
       end
 
       def remove_constraints(routes)
-        routes.each{ |r| r.slice!(/ \{(.*)}/) }
+        routes.each { |r| r.slice!(/ \{(.*)}/) }
       end
-
     end
-
   end
 end
